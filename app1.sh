e4de95118ae5de9bc0651aaa0d90a76bf38994e9
Shivakant Dubey <shivakant395@gmail.com>
2023-11-27 10:25:48 +0530

    Update app1.sh

--- app1.sh
+++ app1.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+# Run the tests
+pytest
+
+# Check the exit code of the pytest command
+if [ $? -eq 0 ]; then
+    # Tests passed, start the app server
+    gunicorn -c server_config.py app0:app
+else
+    # Tests failed, display an error message
+    echo "Tests failed. App server not started."
+    exit 1
+fi
 
